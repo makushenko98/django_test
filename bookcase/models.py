26035ebbae31e204b6530dff5b355737c4734ed1
Georgij Makushenko <g.makushenko@dunice.net>
2019-05-16 17:17:34 +0300

    Add models for Books and Annotations

--- bookcase/models.py
+++ bookcase/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Book(models.Model):
+    titles = models.CharField(max_length=50)
+    number_of_pages = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Annotation(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    text = models.CharField(500)
+    page = models.IntegerField()
+    annotation_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Create your models here.

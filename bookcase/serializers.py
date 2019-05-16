276e0665ad9876cc0f22b178acfc6ea9fdd7b192
Georgij Makushenko <g.makushenko@dunice.net>
2019-05-16 18:48:17 +0300

    Rewrite Bookserialize create method to autofill author with current user

--- bookcase/serializers.py
+++ bookcase/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.serializers import (
     ModelSerializer,
 )
@@ -8,15 +9,32 @@ from bookcase.models import (
 )
 
 
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        fields = ('id', 'username')
+        model = User
+
+
 class BookSerializer(ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        if request is None:
+            return None
+        return self.Meta.model.objects.create(
+            author=request.user,
+            title=validated_data.get('title'),
+            number_of_pages=validated_data.get('number_of_pages')
+        )
 
     class Meta:
         fields = ('id', 'title', 'number_of_pages', 'author')
         model = Book
 
 
-
 class AnnotationSerializer(ModelSerializer):
+    annotation_author = AuthorSerializer()
 
     class Meta:
         fields = ('id', 'page', 'text', 'book', 'annotation_author')

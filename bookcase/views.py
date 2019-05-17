287df215c53ac936732686bf64baf58849aa7156
Georgij Makushenko <g.makushenko@dunice.net>
2019-05-17 10:14:08 +0300

    Make it so only author could edit book entries

--- bookcase/views.py
+++ bookcase/views.py
@@ -1,17 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from bookcase.models import (
     Book,
     Annotation
 )
-
 from bookcase.serializers import (
     BookSerializer,
     AnnotationSerializer,
 )
+from bookcase.permissions import (
+    IsAuthorOrReadOnly,
+)
 
 
 class BookViewSet(ModelViewSet):
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    )
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
